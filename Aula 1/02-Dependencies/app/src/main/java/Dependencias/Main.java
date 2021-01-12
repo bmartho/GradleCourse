53c4cdcd8d55f1bb135f66e146baff8a2396fb9b
Bruno Martho <bjmartho@msn.com>
2021-01-11 23:09:20 -0300

    adding dependencies exercise

---
+++ Aula 1/02-Dependencies/app/src/main/java/Dependencias/Main.java
@@ -0,0 +1,23 @@
+package Dependencias;
+
+import com.google.common.io.Files;
+import java.io.IOException;
+import java.io.File;
+
+public class Main {
+    public void createFile() {
+        String fileName = "newfile.txt";
+        File file = new File(fileName);
+        
+        String content = "text content";
+        try {
+			Files.write(content.getBytes(), file);
+		} catch (IOException exception) {
+			return;
+		}
+    }
+
+    public static void main(String[] args) {
+        new Main().createFile();
+    }
+}

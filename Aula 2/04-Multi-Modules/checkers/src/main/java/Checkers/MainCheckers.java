2efcabf330dcd5b1fc2760b6d72d9c0c472deab2
Bruno Martho <bjmartho@msn.com>
2021-01-18 23:16:49 -0300

    adding exercise 4 in class 2

---
+++ Aula 2/04-Multi-Modules/checkers/src/main/java/Checkers/MainCheckers.java
@@ -0,0 +1,7 @@
+package Checkers;
+public class MainCheckers {
+    public void playCheckers() {
+		System.out.println("MODULE CHECKERS WAS INITIATED");
+		System.out.println("LET'S PLAY CHECKERS");
+    }
+}
---
+++ Aula 2/04-Multi-Modules/chess/src/main/java/Chess/MainChess.java
@@ -0,0 +1,7 @@
+package Chess;
+public class MainChess {
+    public void playChess() {
+		System.out.println("MODULE CHESS WAS INITIATED");
+		System.out.println("LET'S PLAY CHESS");
+    }
+}
